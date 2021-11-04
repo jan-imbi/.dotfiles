c61f542ae14b65346eb6f140c1721b5b4befcb22
Jan Meis <meis@imbi.uni-heidelberg.de>
2021-11-04 11:04:43 +0100

    reorganizing debian setup

--- manual_setup.sh
+++ manual_setup.sh
@@ -1,3 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt install vim nano wget perl git emacs x11-xkb-utils gnupg
+
+ssh-keygen -t ed25519 -C "meis@imbi.uni-heidelberg.de"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+git clone git@github.com:jan-imbi/.dotfiles.git
--- setup-debian.sh
+++ setup-debian.sh
@@ -1,12 +1,8 @@
-ssh-keygen -t ed25519 -C "meis@imbi.uni-heidelberg.de"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
 wget -O jranke.asc "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xe19f5f87128899b192b1a2c2ad5f960a256a04af"
 sudo apt-key add jranke.asc
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt install -t bullseye-cran40 r-base
-git clone git@github.com:jan-imbi/.dotfiles.git
 cd .dotfiles/
 sh deploy.sh
 cd /usr/share/fonts
