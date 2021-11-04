6c1dc7408ad775d423eb51d9afa5bbc75d669a81
Jan Meis <meis@imbi.uni-heidelberg.de>
2021-11-04 11:00:55 +0100

    reorganizing debian setup

---
+++ manual_setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install vim nano wget perl git emacs x11-xkb-utils gnupg
--- setup-debian.sh
+++ setup-debian.sh
@@ -1,7 +1,3 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install vim nano wget perl git emacs x11-xkb-utils gnupg
-
 ssh-keygen -t ed25519 -C "meis@imbi.uni-heidelberg.de"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
